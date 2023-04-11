33d8d87800e7a20f73e8d344c1bdbcd1b62d102c
Николай Челюканов <nikochelyukanov@gmail.com>
2023-04-11 10:51:18 +0300

    Models_version_1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=256)
+    slug = models.SlugField('Сокращенное название жанра',
+                            unique=True, max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=256)
+    slug = models.SlugField('Сокращенное название категории',
+                            unique=True, max_length=50)
+
+
+class Title(models.Model):
+    name = models.CharField('Название произведения', max_length=256)
+    year = models.IntegerField('Год выхода')
+    rating = models.FloatField('Рейтинг приозведения')
+    description = models.TextField('Описание произведения',
+                                   blank=True, null=True)
+    genre = models.ManyToManyField(
+        Genre, through='GenreTitle', on_delete=models.SET_NULL,
+        blank=True, null=True, related_name='titles')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        blank=True, null=True, related_name='titles')
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
