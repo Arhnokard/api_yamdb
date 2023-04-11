be168dc3b593f33356fd00f2cbe2b8d5735ec10c
Николай Челюканов <nikochelyukanov@gmail.com>
2023-04-11 16:28:56 +0300

    Views_version_1

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+import datetime as dt
+
+
+from reviews.models import Genre, Category, Title
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+    description = serializers.CharField(required=False)
+
+    class Meta:
+        model = Title
+        fields = ('__all__')
+        read_only_fields = ('rating',)
+
+    def validate_year(self, value):
+        year = dt.date.today().year
+        if not (0 < value <= year):
+            raise serializers.ValidationError(
+                'Проверьте год создания произведения!')
+        return value
+
+    def validate_genre(self, value):
+        genres = Genre.objects.all()
+        if value not in genres:
+            raise serializers.ValidationError(
+                'Такого жанра не существует!')
+        return value
+
+    def validate_category(self, value):
+        categories = Category.objects.all()
+        if value not in categories:
+            raise serializers.ValidationError(
+                'Такой категории не существует!')
+        return value
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from rest_framework import viewsets
+from api.filters import TitleFilter
+from reviews.models import Genre, Category, Title
+from .serializers import GenreSerializer, CategorySerializer, TitleSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes =
+    lookup_field = 'slug'
+    pagination_class = LimitOffsetPagination
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes =
+    lookup_field = 'slug'
+    pagination_class = LimitOffsetPagination
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    pagination_class = LimitOffsetPagination
+    filterset_class = TitleFilter
+    
