4bb3752fd89ba11cb040b636e74c424997b5a8a1
Евгений Поздняков <Arhnokard@gmail.com>
2023-04-12 17:18:29 +0300

    Feature Rewiew v1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    '''Модель отзывов на произведения'''
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='rewiews')
+    title = models.ForeignKey(Title, on_delete=models.CASCADE,
+                              related_name='rewiews')
+    score = models.IntegerField(
+        validators=[MaxValueValidator(10, 'Оценка может быть от 1 до 10'),
+                    MinValueValidator(1, 'Оценка  может быть от 1 до 10')]
+    )
+    pub_date = models.TimeField('Дата отзыва',auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:15]
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['author', 'title'],
+                                    name='unique_author')
+        ]
