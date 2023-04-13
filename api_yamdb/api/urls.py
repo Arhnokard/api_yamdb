5bff9fd6a54289323681ab0cecc1574fd3d53d9c
Николай Челюканов <nikochelyukanov@gmail.com>
2023-04-13 10:13:57 +0300

    Endpoints_version_2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 from .views import GenreViewSet, CategoryViewSet, TitleViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register('genres', GenreViewSet, basename='genres')
-router_v1.register('categories', CategoryViewSet, basename='categories')
-router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('genres', GenreViewSet)
+router_v1.register('categories', CategoryViewSet)
+router_v1.register('titles', TitleViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
