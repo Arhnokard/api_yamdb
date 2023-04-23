342022fb17e39fadc932e5f905bfc124c0f368d9
Евгений Поздняков <Arhnokard@gmail.com>
2023-04-23 21:25:55 +0300

    Создан файл имитирующий env

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,15 +3,13 @@ from pathlib import Path
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
+from .env import key
 
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('key')
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
